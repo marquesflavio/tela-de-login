6a57afa8bebcc66e9e6517464f819624a4ab88c7
Flávio Marques <flavio.topmed@outlook.com>
2024-07-27 10:56:06 -0300

    installed ErrorMessage lib from hook form

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ O objetivo desse projeto é efetuar uma página de login;
 ## Requisitos
 
 Necessário possuir validação de e-mail e senha;
+ - Utilizei o hook-form para validações de form.
+    - necessário instalar o hook-form e a lib de errorsMessage do hook-form;
 Utilizar react-router-dom para a navegação entra páginas;
 
 ## Bonus
--- src/components/FormularioLogin/FormularioLoginView.jsx
+++ src/components/FormularioLogin/FormularioLoginView.jsx
@@ -1,20 +1,48 @@
+import { useForm } from 'react-hook-form';
 import './FormularioLoginStyle.css';
+import { ErrorMessage } from '@hookform/error-message';
 
 const FormularioLoginView = () => {
+    const {
+        register,
+        formState: { errors },
+        handleSubmit } = useForm();
+
+    const onSubmit = (e) => {
+        console.log(e)
+    }
 
     return (
         <div>
             <h1>Formulario de Login</h1>
 
-            <div className="divInput">
-                <p>Login</p>
-                <input type="text" placeholder="Email" className="input" />
-                <p>Senha</p>
-                <input type="password" placeholder="Senha" className="input" />
-            </div>
-            <div className="divButton">
-                <button>Entrar</button>
-            </div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div className="divInput">
+                    <p>Login</p>
+                    <input 
+                        type="text" 
+                        placeholder="Email" 
+                        className="input" 
+                        {...register('email', { required: "Email obrigatório" })}
+                    />
+
+                    <ErrorMessage errors={errors} name="email" className="errorMessage"/>
+                  
+                    <p>Senha</p>
+                    <input 
+                        type="password" 
+                        placeholder="Senha" 
+                        className="input" 
+                        {...register('password', { required: "Senha obrigatória" })}
+                    />
+
+                    <ErrorMessage errors={errors} name="password" className="errorMessage"/>
+               
+                </div>
+                <div className="divButton">
+                    <button type='submit'>Entrar</button>
+                </div>
+            </form>
         </div>
     )
 };
